ee96f9638886fcf635be9d5fb860047cbe5db985
Ankylotech <34301208+Ankylotech@users.noreply.github.com>
2018-02-04 20:49:42 +0100

    plotter verbessert und Staerke variable
    plotter nimmt autoatisch den neusten Graphen

--- plotter.py
+++ plotter.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 from ast import literal_eval as make_tuple
-
+import numpy as np
+myarray = np.fromfile('saveDataIndex.dat',dtype=float)
+myIndex = 0
+for x in myarray:
+    myIndex += x
 x = []
 y = []
-with open("C:\\Users\\Anky\Desktop\\Programmieren\\Processing\\EvoSim\\durchschnittsLw1.txt") as ältestes:
+with open("C:\\Users\\Anky\Desktop\\Programmieren\\Processing\\EvoSim\\ältestesLw%s.txt"% myIndex) as ältestes:
     data = ältestes.read().split(";")
     data.pop(-1)
     for i in data:
@@ -11,3 +15,24 @@ with open("C:\\Users\\Anky\Desktop\\Programmieren\\Processing\\EvoSim\\durchschn
         y.append(make_tuple(i)[1])
     plt.plot(x,y)
     plt.show()
+x = []
+y = []
+with open("C:\\Users\\Anky\Desktop\\Programmieren\\Processing\\EvoSim\\durchschnittsLw%s.txt"% myIndex) as ältestes:
+    data = ältestes.read().split(";")
+    data.pop(-1)
+    for i in data:
+        x.append(make_tuple(i)[0])
+        y.append(make_tuple(i)[1])
+    plt.plot(x,y)
+    plt.show()
+x = []
+y = []
+with open("C:\\Users\\Anky\Desktop\\Programmieren\\Processing\\EvoSim\\staerkeLw%s.txt"% myIndex) as ältestes:
+    data = ältestes.read().split(";")
+    data.pop(-1)
+    for i in data:
+        x.append(make_tuple(i)[0])
+        y.append(make_tuple(i)[1])
+    plt.plot(x,y)
+    plt.show()
+
